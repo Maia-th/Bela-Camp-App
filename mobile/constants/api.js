2f934d30dd89b112129ccc8a610e41adad4b4d8e
Maia-Th <thiagomaia633@gmail.com>
2025-08-30 11:52:16 -0300

    fix: Ajuste na Url do backend

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,3 +1,5 @@
-// if you're using your physical phone, change this to the deployed url
-// we have explained this in the course :-)
-export const API_URL = "http://localhost:5001/api";
+/*Emulador PC
+export const API_URL = "http://localhost:5001/api";*/
+
+/*Dispositivo Fisico - Atualize o IP */
+export const API_URL = "http://192.168.2.7:5001/api";
